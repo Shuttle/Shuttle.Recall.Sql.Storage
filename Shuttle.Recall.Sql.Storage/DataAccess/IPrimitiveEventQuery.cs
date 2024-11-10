cfe6f1c89d77c8c96639853a03aeff7092c64026
Eben <me@ebenroux.co.za>
2024-11-10 08:42:49 +0200

    Update IPrimitiveEventQuery.cs

--- Shuttle.Recall.Sql.Storage/DataAccess/IPrimitiveEventQuery.cs
+++ Shuttle.Recall.Sql.Storage/DataAccess/IPrimitiveEventQuery.cs
@@ -1,5 +1,9 @@
-﻿namespace Shuttle.Recall.Sql.Storage;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Shuttle.Recall.Sql.Storage;
 
 public interface IPrimitiveEventQuery
 {
+    Task<IEnumerable<PrimitiveEvent>> SearchAsync(PrimitiveEventSpecification specification);
 }
